9e95f75e239139f4829c84ec8db338ac2794375e
chasehubbard <79477203+chasehubbard@users.noreply.github.com>
2021-03-22 16:57:32 -0400

    Update USPSAPI.py

--- USPSAPI.py
+++ USPSAPI.py
@@ -13,17 +13,8 @@ def USPS():
     Zip_origin = '22201'
 
 
-    Types_of_Orders = ['First Class','Priority','First Class Commercial','First Class'
-,'First Class Commercial','First Class HFP Commercial','Parcel Select Ground'
-,'Priority','Priority Commercial','Priority Cpp'
-,'Priority HFP Commercial','Priority HFP CPP','Priority Mail Express'
-,'Priority Mail Express Commercial','Priority Mail Express CPP','Priority Mail Express Sh','Priority Mail Express Sh Commercial'
-,'Priority Mail Express HFP','Priority Mail Express HFP Commercial','Priority Mail Express HFP CPP'
-,'Priority Mail Cubic','Retail Ground','Media','Library'
-,'All','Online','Plus','BPM'
-    ]
 
-    Tested = ['PRIORITY, PRIORITY MAIL EXPRESS,Parcel Select Ground,Media ']
+
 #priority is 3 day, priority mail express is 2 day, parcel select ground is longer,Media is slower unknown time but cheaper 
 #this is the actual request structure for USPS's API
     requestXML = """
