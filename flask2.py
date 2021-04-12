f7688fd052dc4f142e76a721f32deb9f58198721
chasehubbard <79477203+chasehubbard@users.noreply.github.com>
2021-04-12 13:49:07 -0400

    Update flask2.py

--- flask2.py
+++ flask2.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, Blueprint
 from flask_restful import Api, Resource, reqparse, abort
 from flask_sqlalchemy import SQLAlchemy
 
+
 import urllib
 import requests
 import xml.etree.ElementTree as ET
@@ -9,64 +10,79 @@ import requests
 import pycurl
 from io import BytesIO
 from bs4 import BeautifulSoup
-def USPS():
+def USPS(weight,width,length,height,zipto,zipfrom):
     usps_username = "850CREAT7421"
     usps_password = "827OY73NU544"
 
     Zip_origin = '22201'
 
+
+
+
+#priority is 3 day, priority mail express is 2 day, parcel select ground is longer,Media is slower unknown time but cheaper
 #this is the actual request structure for USPS's API
     requestXML = """
 <RateV4Request USERID="850CREAT7421">
-
 <Revision>2</Revision>
-
 <Package ID="0">
-
-<Service>PRIORITY</Service>
-
+<Service>ALL</Service>
 <ZipOrigination>30301</ZipOrigination>
-
 <ZipDestination>26301</ZipDestination>
-
 <Pounds>8</Pounds>
-
 <Ounces>2</Ounces>
-
 <Container></Container>
-
 <Width></Width>
-
 <Length></Length>
-
 <Height></Height>
-
 <Girth></Girth>
-
 <Machinable>TRUE</Machinable>
-
 </Package>
-
 </RateV4Request>
     """
 
 #convert XML to a webpage and then search the webpage using python
-    docString = requestXML
+
+    docString = BeautifulSoup(requestXML,'xml')
+    docString.Pounds.string = weight
+    docString.Width.string = width
+    docString.Length.string = length
+    docString.Height.string = height
+    docString.ZipOrigination.string = zipfrom
+    docString.ZipDestination.string = zipto
+    docString = str(docString)
     docString = docString.replace('\n','').replace('\t','')
     docString = urllib.parse.quote_plus(docString)
+
+
 
     url = "https://secure.shippingapis.com/ShippingAPI.dll?API=RateV4&XML=" + docString
 #print(url + "\n\n")
 
     response = requests.get(url)
 
+#print(response)
 
+#print(response.text)
 
     soup = BeautifulSoup(response.text, 'xml')
-
     price_string = soup.find('Rate')
-    price = float(price_string.text.strip())
-    return price
+    prices = soup.find_all('Rate')
+    new_prices = []
+    real_service_types = []
+    sub_service_types = []
+    service_types = soup.find_all('MailService')
+    for x in range(len(service_types)):
+        sub_service_types.append(service_types[x].text.split('&'))
+    for x in range(len(sub_service_types)):
+            real_service_types.append(sub_service_types[x])
+
+    for x in range(len(prices)):
+        try:
+            testtuple = (float(prices[x].text.strip())), (real_service_types[x][0] + real_service_types[x][5][3:])
+        except:
+            testtuple = (float(prices[x].text.strip())), (real_service_types[x][0])
+        new_prices.append(testtuple)
+    return(new_prices)
 
 
 
@@ -152,10 +168,11 @@ def get_zipfrom():
 @app.route('/price/')
 def price():
     price_data = []
-    price_data.append(USPS())
+    price_data.append(USPS(wieghts[0],lengths[0],heights[0],widths[0],zipto_locations[0],zipfrom_locations[0]))
     return jsonify(price_data)
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
