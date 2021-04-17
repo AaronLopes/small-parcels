1ed7ec7bc50230a1a04429cd5b2479a75a393f5a
chasehubbard <chase.a.hubbard@gmail.com>
2021-04-17 13:08:21 -0400

    changes

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { View, Button } from 'react-native';
+import axios from 'axios'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+var url = "http://192.168.1.79:5000/get_zipto";
+
+export default class PrevCommune extends Component {
+    constructor(props){
+        super(props);
+        this.navigation=this.props.navigation;
+    };
+
+    getAxios=()=>{
+        axios.get(`${url}`).then((response)=>{
+            console.log("succes axios :",response);
+        }).catch((error)=>{
+            console.log("fail axios :", error);
+        });
+    };
+
+    getFetch=()=>{
+        fetch(url).then((response)=>{
+            console.log("succes fetch :",response)
+        }).catch((error)=>{
+            console.log("fail fetch :",error)
+        })
+    }
+
+    render(){
+        return (
+            <View>
+                <Button onPress={()=>this.getAxios()} title={"get axios"}></Button>
+
+                <Button onPress={()=>this.getFetch()} title={"get fetch"}></Button>
+            </View>
+        );
+    };
+};
